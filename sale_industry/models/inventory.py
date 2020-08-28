b71c0afbbda1e53ab6f3a867c68f424fc522a5a7
Eslam <eslam.rezk96@hotmail.com>
2020-08-28 14:05:08 +0200

    inventory

--- sale_industry/models/inventory.py
+++ sale_industry/models/inventory.py
@@ -5,6 +5,6 @@ from odoo import models, fields, api
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    sale_order_line_id = fields.Many2one(comodel_name="sale.order.line")
-    dimension = fields.Integer(string="Dimension", required=False, related='sale_order_line_id.dimension')
-    size = fields.Integer(string="Size", required=False, related='sale_order_line_id.size')
+    dimension_id = fields.Many2one(comodel_name="sale.order.line",)
+    dimension = fields.Integer(string="Dimension",store=True, related='dimension_id.dimension')
+    size = fields.Integer(string="Size",)
--- sale_industry/models/sale_order.py
+++ sale_industry/models/sale_order.py
@@ -5,5 +5,5 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order.line'
 
-    dimension = fields.Integer(string="Dimension", required=False, )
+    dimension = fields.Integer(string="Dimension", required=False, store=True, )
     size = fields.Integer(string="Size", required=False, )
